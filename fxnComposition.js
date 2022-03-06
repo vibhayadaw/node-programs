3f6b317585a5a4d40247ae3fb187a1a4d5b7fdcb
Vibha Singh <vibha.singh@microfocus.com>
2022-03-06 21:38:38 +0530

    update fxnCompostion

--- fxnComposition.js
+++ fxnComposition.js
@@ -8,17 +8,20 @@ const triple = x => x * 3;
 
 const doubleOfTen = double(10); //20
 const sixTimesTen = triple(doubleOfTen); //60
+// const compose = (...funcs) =>
+//   initialArg => funcs.reduce((acc, func) => func(acc), initialArg);
+const compose = (...funcs) => {
+  console.log(funcs);
+  return (initialArg) => {
+    console.log(initialArg);
+    return funcs.reduce((acc, func) => func(acc), initialArg);
+  }
+}
+  
 
 const sixTimes = compose(double, triple);
 console.log(sixTimes(10)) //60
 
-function compose (f,g) {
-  return (x) => f(g(x))
-}
-
-// function compose(...args){
-//   if(args.length === 1){
-//     return (x)=>(args[0](x))
-//   }
-//   return (x) => args[0]( compose(args[1..]))
+// function compose (f,g) {
+//   return (x) => f(g(x))
 // }
