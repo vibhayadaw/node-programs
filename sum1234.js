e10a0694c5b1d0dc07d090e52bd4b05215e662b5
Vibha Singh <vibha.singh@microfocus.com>
2022-03-14 13:36:28 +0530

    update sum recursion

--- sum1234.js
+++ sum1234.js
@@ -1,10 +1,12 @@
-let sum = function (a){
-    return function(b){
-        if(b === undefined)
-            return a;
-        else{
-          return sum(a + b)
-        }
-    }
-}
+// let sum = function (a){
+//     return function(b){
+//         if(b === undefined)
+//             return a;
+//         else{
+//           return sum(a + b)
+//         }
+//     }
+// }
+let sum = (a) => (b) => (b === undefined)? a : sum(a+b)
+
 console.log(sum(1)(2)(3)())
